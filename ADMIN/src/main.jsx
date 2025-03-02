a613133ea7ba1683dc59640a5101140241c3df73
Kishore <makam.kishore.991.sandhya@gmail.com>
2025-03-02 17:56:01 +0530

    new commit

--- ADMIN/src/main.jsx
+++ ADMIN/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import AdminContextProvider from './contexts/adminContext.jsx'
+import AdminContextProvider from './contexts/AdminContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
