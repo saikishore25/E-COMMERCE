a4dcb875fe98bac6c42fc8f4ff99d8b26a98c13c
Kishore <makam.kishore.991.sandhya@gmail.com>
2025-03-02 17:46:13 +0530

    yes commit

--- ADMIN/src/components/AdminNavBar.jsx
+++ ADMIN/src/components/AdminNavBar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import logo_png from "../assets/logo.png"
 import { useContext } from 'react'
-import { AdminContext } from '../contexts/adminContext'
+import { AdminContext } from '../contexts/AdminContext.jsx'
 import { useNavigate } from "react-router-dom";  // ✅ Fix: Add navigate
 
 const AdminNavBar = () => {
