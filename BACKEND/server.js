dafb8d8cdc1b8e016a1b8a63b0497cfe018000b9
Kishore <makam.kishore.991.sandhya@gmail.com>
2025-04-04 17:17:46 +0530

    new commit

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -21,7 +21,7 @@ const corsOptions = {
     origin: [
         "http://localhost:5173",
         "http://localhost:5174",
-        "https://e-commerce-admin-six-henna.vercel.app",
+        "https://e-commerce-admin-mu-ashy.vercel.app",
         "https://e-commerce-frontend-theta-eight.vercel.app"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
