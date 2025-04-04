63b5acaf48be1a1f59659f3801f7ee197560f29d
Kishore <makam.kishore.991.sandhya@gmail.com>
2025-04-04 17:08:34 +0530

    commit again

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -18,13 +18,18 @@ app.use(express.urlencoded({extended:true}))
 app.use(cookieParser());
 
 const corsOptions = {
-
-    origin: ["https://e-commerce-seven-lake-85.vercel.app/"], 
+    origin: [
+        "http://localhost:5173",
+        "http://localhost:5174",
+        "https://e-commerce-admin-six-henna.vercel.app",
+        "https://e-commerce-seven-lake-85.vercel.app"
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
-}
+};
+app.use(cors(corsOptions));
 
-app.use(cors(corsOptions))
 
 
 mongoConnect()
