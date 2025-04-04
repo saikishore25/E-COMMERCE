378ba9c4c1303c06c7e7f4dc4e444c233d47e497
Kishore <makam.kishore.991.sandhya@gmail.com>
2025-04-04 17:03:12 +0530

    commit2

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 
 const corsOptions = {
 
-    origin: ["http://localhost:5173","http://localhost:5174"], 
+    origin: ["https://e-commerce-seven-lake-85.vercel.app/"], 
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }
