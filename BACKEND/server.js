8e9a65b15c869c2ab8431024338842dbd2ea6930
Kishore <makam.kishore.991.sandhya@gmail.com>
2025-04-04 16:37:38 +0530

    new commit

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 
 const corsOptions = {
 
-    origin: ["http://localhost:5173","http://localhost:5174"], 
+    origin: ["http://localhost:5173","http://localhost:5174", "https://e-commerce-admin-bice.vercel.app/", "https://e-commerce-frontend-ivory-five.vercel.app/"], 
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }
